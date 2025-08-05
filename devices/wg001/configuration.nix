dc1793cb6dfcc5459ed4effdb89325e6f020d303
Ameer Taweel <ameer.taweel@mada.ps>
2025-08-05 08:38:53 +0000

    wg001: bash wezterm integration

--- devices/wg001/configuration.nix
+++ devices/wg001/configuration.nix
@@ -30,6 +30,10 @@
 
   programs.direnv.enable = true;
 
+  programs.bash.interactiveShellInit = ''
+    source "${pkgs.wezterm}/etc/profile.d/wezterm.sh"
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
