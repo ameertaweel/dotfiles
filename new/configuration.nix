c9b1d259ed0e53852a4ee8e00b9e617ae66b3329
Ameer Taweel <ameertaweel2002@gmail.com>
2026-03-25 00:25:36 +0200

    enable gnome and gdm

--- new/configuration.nix
+++ new/configuration.nix
@@ -82,8 +82,8 @@ let
       services.xserver.enable = false;
 
       # Enable the GNOME Desktop Environment.
-      # services.displayManager.gdm.enable = true;
-      # services.desktopManager.gnome.enable = true;
+      services.displayManager.gdm.enable = true;
+      services.desktopManager.gnome.enable = true;
 
       hardware.bluetooth.enable = true;
 
