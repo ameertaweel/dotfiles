560fa4f6f00091d83876306c0c02f5fc9a2ffdb1
Ameer Taweel <ameertaweel2002@gmail.com>
2026-04-06 01:24:23 +0300

    fix rebuilv4 command

--- new/rebuildv4.sh
+++ new/rebuildv4.sh
@@ -1,1 +1,1 @@
-sudo NIX_PATH="" nixos-rebuild --log-format bar-with-logs --file configuration.nix --attr myHost boot
+sudo NIX_PATH="" nixos-rebuild --log-format bar-with-logs --file system.nix --attr myHost boot
